a5db8c0714e74c51a898f134c099508adeae9568
Bartosz Żabiński <bartosz.zabinski@profil-software.com>
2017-10-11 20:52:41 +0200

    changed models

--- mysite/engApp/models.py
+++ mysite/engApp/models.py
@@ -2,3 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Word(models.Model):
+    word_pl = models.CharField(max_length=30)
+    word_en = models.CharField(max_length=30)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+
+
+class Student(User):
+    pass
+
+
+class Profile(models.Model):
+    id_student = models.ForeignKey(Student)
+    level = models.CharField(max_length=2)
